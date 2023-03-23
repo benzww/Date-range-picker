dc8a950b0cb3023362b4e9b99f4e3ba018b8dae8
benzww <shalinibenze@gmail.com>
2023-03-23 17:45:01 +0530

    finished project.

---
+++ app/src/main/java/com/example/daterangepicker/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.daterangepicker
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.daterangepicker.databinding.ActivityMainBinding
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.text.SimpleDateFormat
+import java.util.*
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            showDataRangePicker()
+        }
+    }
+    @SuppressLint("SetTextI18n")
+    private fun showDataRangePicker() {
+        val dateRangePicker = MaterialDatePicker.Builder
+            .dateRangePicker()
+            .setTitleText("Select Date")
+            .build()
+
+        dateRangePicker.show(
+            supportFragmentManager,
+            "date_range_picker"
+        )
+        dateRangePicker.addOnPositiveButtonClickListener { dateSelected ->
+
+            val startDate = dateSelected.first
+            val endDate = dateSelected.second
+
+            if (startDate != null && endDate != null) {
+                binding.tvRangeDate.text =
+                    "Reserved\nStartDate: ${convertLongToTime(startDate)}\n" +
+                            "EndDate: ${convertLongToTime(endDate)}"
+            }
+        }
+
+    }
+    private fun convertLongToTime(time: Long): String {
+        val date = Date(time)
+        val format = SimpleDateFormat(
+            "dd.MM.yyyy",
+            Locale.getDefault())
+        return format.format(date)
+    }
+}
